6a1cc35cad708974d87c99a4146053da1897cd37
Kübranur Şahin <78857206+kubranursahin@users.noreply.github.com>
2022-05-14 16:08:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            //ihtiyacKrediManager.Hesapla();
+            //ihtiyacKrediManager.Guncelle();
+
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            //konutKrediManager.Hesapla();
+            //konutKrediManager.Guncelle();
+
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            //tasitKrediManager.Hesapla();
+            //tasitKrediManager.Guncelle();
+            ILoggerService dataBaseLoggerService = new DataBaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+           
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager, dataBaseLoggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, konutKrediManager };
+            //basvuruManager.KrediOnBilgilendirmeYap(krediler);
+        }
+
+    }
+}
